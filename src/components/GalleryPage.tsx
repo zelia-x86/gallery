4dca01bf9852b43092613f93d2ac50553d192d54
zelia-x86 <209870153+zelia-x86@users.noreply.github.com>
2026-07-13 16:37:44 +0000

    some glossery

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -6,17 +6,18 @@ import { useSearchParams } from "next/navigation"
 import { Separator } from "./ui/separator";
 import fetchJson from "@/hooks/fetchJson";
 import { Loading, states } from "./Loading";
+import { useEffect } from "react";
 
 export interface galleryJSON {
-  title: string,
+  title?: string,
   albums: string[],
   images: string[],
 }
 
-function Album ({href}: {href: string}) {
+function Album ({title, source}: {title: string, source: URL}) {
   return (
-    <Link href={"/gallery?link=" + encodeURIComponent(href)}>
-      <Image src={href + "/cover.jpg"} alt="loading?"
+    <Link href={"/gallery?link=" + encodeURIComponent(`${source.href}/${title}`)}>
+      <Image src={`${source.href}/${title}/cover.jpg`} alt={title}
         className="aspect-video m-2 min-h-32 min-w-32 max-h-96 max-w-96 hover:bg-sky-950
           transition-colors duration-500 bg-black
           object-contain rounded-lg border-2 border-[#03fbff]"
@@ -29,10 +30,10 @@ function Album ({href}: {href: string}) {
   )
 }
 
-function File ({source, image, i}: {source: string, image: string, i: number}) {
+function File ({source, image, i}: {source: URL, image: string, i: number}) {
   return (
-    <Link href={`/view?link=${encodeURIComponent(source)}&i=${i}`}>
-      <Image src={`${source}/${image}`} alt="loading?"
+    <Link href={`/view?link=${encodeURIComponent(source.href)}&i=${i}`}>
+      <Image src={`${source.href}/${image}`} alt={image}
         className="aspect-video m-2 min-h-32 min-w-32 max-h-96 max-w-96 hover:bg-sky-950
           transition-colors duration-500 bg-black
           object-contain rounded-lg border-2 border-[#03fbff]"
@@ -47,25 +48,25 @@ function Label ({children}: {children: string}) {
   )
 }
 
-function Page ( {json}: {json: galleryJSON} ) {
-  const source = new URL(decodeURIComponent(useSearchParams().get("link") ?? ""));
-
+function Page ( {source, json}: {source: URL, json: galleryJSON} ) {
+  useEffect(() => {
+    if (json.title)
+      document.title += ` : ${json.title}`;
+  }, [json]);
   return (
     <div className="absolute w-screen h-screen rounded-3xl p-6 inset-0 border-4
       border-[#03fbff] overflow-y-scroll overflow-x-hidden">
       <Label>{"Album: " + json.albums.length.toString()}</Label>
       <div className="flex flex-wrap rounded-lg ">
         {json.albums.map((album, i) => {
-          const newSource = new URL(source);
-          newSource.pathname += "/" + album;
-          return ( <Album href={newSource.href} key={i} /> )
+          return ( <Album title={album} source={source} key={i} /> )
         })}
       </div>
       <Separator className="m-3" />
       <Label>{"Images: " + json.images.length.toString()}</Label>
       <div className="flex flex-wrap rounded-lg">
         {json.images.map((image, i) => 
-          <File source={source.href} image={image} i={i + 1} key={i}  />
+          <File source={source} image={image} i={i + 1} key={i}  />
         )}
       </div>
     </div>
@@ -80,7 +81,7 @@ export default function GalleryPage () {
   const {state, json} = fetchJson <galleryJSON> (link + "/index.json");
 
   if (state == states.loaded && json != null)
-    return ( <Page json={json} /> )
+    return ( <Page source={new URL(link)} json={json} /> )
   else
     return ( <Loading state={state} /> )
 }
