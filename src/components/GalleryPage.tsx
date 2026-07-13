c1b0ac9f36d2e204e39590a27a581170f9caf11b
zelia-x86 <209870153+zelia-x86@users.noreply.github.com>
2026-07-13 15:36:33 +0900

    testing ui

--- src/components/GalleryPage.tsx
+++ src/components/GalleryPage.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation"
+import { Separator } from "./ui/separator";
 
 export interface galleryJSON {
   title: string,
@@ -12,55 +13,59 @@ export interface galleryJSON {
 
 function Album ({href}: {href: string}) {
   return (
-    <Link className="p-2 " href={"/gallery?link=" + encodeURIComponent(href)}>
+    <Link href={"/gallery?link=" + encodeURIComponent(href)}>
       <Image src={href + "/cover.jpg"} alt="loading?"
-        className="hover:bg-sky-950 transition-colors duration-500 bg-black
-          object-contain rounded-lg border-2 border-[#03fbff] h-full w-full"
-        width={96} height={96} />
-    </Link>    
+        className="aspect-video m-2 min-h-32 min-w-32 max-h-96 max-w-96 hover:bg-sky-950
+          transition-colors duration-500 bg-black
+          object-contain rounded-lg border-2 border-[#03fbff]"
+        width={96} height={96}
+        // onError={(e) => {
+        //   (e.target as HTMLImageElement).src = "";
+        // }}
+      />
+    </Link>
   )
 }
 
 function File ({source, image, i}: {source: string, image: string, i: number}) {
   return (
-    <Link className="p-2" href={`/view?link=${encodeURIComponent(source)}&i=${i}`}>
+    <Link href={`/view?link=${encodeURIComponent(source)}&i=${i}`}>
       <Image src={`${source}/${image}`} alt="loading?"
-        className="hover:bg-sky-950 transition-colors duration-500 bg-black
-          object-contain rounded-lg border-2 border-[#03fbff] h-full w-full"
+        className="aspect-video m-2 min-h-32 min-w-32 max-h-96 max-w-96 hover:bg-sky-950
+          transition-colors duration-500 bg-black
+          object-contain rounded-lg border-2 border-[#03fbff]"
         width={96} height={96} />
     </Link>
   )
 }
 
-function Separator ({flag}: {flag: boolean}) {
-  if (flag)
-    return ( <h1>***********************************************</h1> )
-  else
-    return ( <></> )
+function Label ({children}: {children: string}) {
+  return (
+    <div className="text-3xl text-white" >{children}</div>
+  )
 }
 
 export function GalleryPage ( {json}: {json: galleryJSON} ) {
   const source = new URL(decodeURIComponent(useSearchParams().get("link") ?? ""));
 
-  const albums = json.albums.map((album, i) => {
-    const newSource = new URL(source);
-    newSource.pathname += "/" + album;
-    return ( <Album href={newSource.href} key={i} /> )
-  })
-
-  const images = json.images.map((image, i) => 
-    <File source={source.href} image={image} i={i + 1} key={i}  />
-  );
-
   return (
-    <>
-      <div className="grid grid-cols-10 auto-rows-auto w-screen gap-2 rounded-lg p-6 lg:overflow-visible">
-        {albums}
+    <div className="absolute w-screen h-screen rounded-3xl p-6 inset-0 border-4
+      border-[#03fbff] overflow-y-scroll overflow-x-hidden">
+      <Label>{"Album: " + json.albums.length.toString()}</Label>
+      <div className="flex flex-wrap rounded-lg ">
+        {json.albums.map((album, i) => {
+          const newSource = new URL(source);
+          newSource.pathname += "/" + album;
+          return ( <Album href={newSource.href} key={i} /> )
+        })}
       </div>
-      <Separator flag={albums.length > 0} />
-      <div className="grid grid-cols-10 auto-rows-auto w-screen gap-2 rounded-lg p-6 lg:overflow-visible">
-        {images}
+      <Separator className="m-3" />
+      <Label>{"Images: " + json.images.length.toString()}</Label>
+      <div className="flex flex-wrap rounded-lg">
+        {json.images.map((image, i) => 
+          <File source={source.href} image={image} i={i + 1} key={i}  />
+        )}
       </div>
-    </>
+    </div>
     )
 }
